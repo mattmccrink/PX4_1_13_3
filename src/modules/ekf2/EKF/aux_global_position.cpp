dagar/PX4-Firmware

WIP: initial ekf2 aux global position fusion

src/modules/ekf2/EKF/aux_global_position.cpp
@@ -1,0 +1,103 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2023 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "ekf.h"
+
+#include "aux_global_position.hpp"
+
+#if defined(CONFIG_EKF2_AUX_GLOBAL_POSITION) && defined(MODULE_NAME)
+
+void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed)
+{
+
+#if defined(MODULE_NAME)
+
+	if (_global_odometry_sub.updated()) {
+
+		global_odometry_s global_odometry{};
+		_global_odometry_sub.copy(&global_odometry);
+
+		const int64_t time_us = global_odometry.timestamp_sample - static_cast<int64_t>(_param_ekf2_agp_delay.get() * 1000);
+
+		AuxGlobalPositionSample sample{};
+		sample.time_us = time_us;
+		sample.latitude = global_odometry.latitude;
+		sample.longitude = global_odometry.longitude;
+		sample.positional_uncertainty = global_odometry.positional_uncertainty;
+		sample.position_valid = global_odometry.valid;
+		sample.altitude = global_odometry.altitude_agl;
+		sample.heading = global_odometry.heading;
+		sample.heading_valid = global_odometry.heading_valid;
+
+		_aux_global_position_buffer.push(sample);
+	}
+
+#endif // MODULE_NAME
+
+
+	AuxGlobalPositionSample aux_global_position_sample;
+
+	if (_aux_global_position_buffer.pop_first_older_than(imu_delayed.time_us, &aux_global_position_sample)) {
+
+		if (!ekf.global_origin_valid()) {
+			return;
+		}
+
+		const Vector2f position = ekf.global_origin().project(aux_global_position_sample.latitude, aux_global_position_sample.longitude);
+		//const float hgt = ekf.getEkfGlobalOriginAltitude() - (float)aux_global_position_sample.altitude;
+
+		estimator_aid_source2d_s aid_src{};
+
+		// relax the upper observation noise limit which prevents bad GPS perturbing the position estimate
+		float pos_noise = math::max(aux_global_position_sample.positional_uncertainty, _param_ekf2_agp_noise.get());
+		const float pos_var = sq(pos_noise);
+		const Vector2f pos_obs_var(pos_var, pos_var);
+		ekf.updateHorizontalPositionAidSrcStatus(aux_global_position_sample.time_us,
+				position,                                   // observation
+				pos_obs_var,                                // observation variance
+				math::max(_param_ekf2_agp_gate.get(), 1.f), // innovation gate
+				aid_src);
+		aid_src.fusion_enabled = _param_ekf2_agp_ctrl.get() & 0b001; // bit 0 Horizontal position
+
+		if (aux_global_position_sample.position_valid && ekf.control_status_flags().yaw_align) {
+			ekf.fuseHorizontalPosition(aid_src);
+		}
+
+#if defined(MODULE_NAME)
+		aid_src.timestamp = hrt_absolute_time();
+		_estimator_aid_src_aux_global_position_pub.publish(aid_src);
+#endif // MODULE_NAME
+	}
+}
+
+#endif // CONFIG_EKF2_AUX_GLOBAL_POSITION

src/modules/ekf2/EKF/aux_global_position.hpp
@@ -1,0 +1,113 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2023 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef EKF_AUX_GLOBAL_POSITION_HPP
+#define EKF_AUX_GLOBAL_POSITION_HPP
+
+// interface?
+//  - ModuleParams
+//  - Base class EKF
+//  -  bool update(imu)
+//   how to get delay?
+//  WelfordMean for init?
+//  WelfordMean for rate
+
+#include "common.h"
+#include "RingBuffer.h"
+
+#if defined(CONFIG_EKF2_AUX_GLOBAL_POSITION) && defined(MODULE_NAME)
+
+#if defined(MODULE_NAME)
+# include <px4_platform_common/module_params.h>
+# include <uORB/PublicationMulti.hpp>
+# include <uORB/Subscription.hpp>
+# include <uORB/topics/estimator_aid_source2d.h>
+# include <uORB/topics/global_odometry.h>
+#endif // MODULE_NAME
+
+class Ekf;
+
+class AuxGlobalPosition : public ModuleParams
+{
+public:
+	AuxGlobalPosition() : ModuleParams(nullptr) {}
+	~AuxGlobalPosition() = default;
+
+	void update(Ekf &ekf, const estimator::imuSample &imu_delayed);
+
+	void updateParameters()
+	{
+		updateParams();
+	}
+
+private:
+
+	struct AuxGlobalPositionSample {
+		uint64_t time_us{};     ///< timestamp of the measurement (uSec)
+		double latitude{};
+		double longitude{};
+		float positional_uncertainty{}; ///< XYZ position uncertainty (m)
+		bool position_valid{false};
+		float altitude{};
+		float heading{};      ///< quaternion defining rotation from body to earth frame
+		bool heading_valid{false};
+	};
+
+	RingBuffer<AuxGlobalPositionSample> _aux_global_position_buffer{20}; // TODO: size with _obs_buffer_length and actual publication rate
+	uint64_t _time_last_buffer_push{0};
+
+	enum state {
+		stopped,
+		starting,
+		active,
+		stopping,
+		resetting,
+	};
+
+#if defined(MODULE_NAME)
+	uORB::PublicationMulti<estimator_aid_source2d_s> _estimator_aid_src_aux_global_position_pub{ORB_ID(estimator_aid_src_aux_global_position)};
+	uORB::Subscription _global_odometry_sub{ORB_ID(global_odometry)};
+
+	DEFINE_PARAMETERS(
+		(ParamInt<px4::params::EKF2_AGP_CTRL>) _param_ekf2_agp_ctrl,
+		(ParamFloat<px4::params::EKF2_AGP_DELAY>) _param_ekf2_agp_delay,
+		(ParamFloat<px4::params::EKF2_AGP_NOISE>) _param_ekf2_agp_noise,
+		(ParamFloat<px4::params::EKF2_AGP_GATE>) _param_ekf2_agp_gate
+	)
+
+#endif // MODULE_NAME
+};
+
+#endif // CONFIG_EKF2_AUX_GLOBAL_POSITION
+
+#endif // !EKF_AUX_GLOBAL_POSITION_HPP

src/modules/logger/logged_topics.cpp
@@ -201,6 +201,9 @@ void LoggedTopics::add_default_topics()
 	// add_optional_topic_multi("estimator_aid_src_optical_flow", 100, MAX_ESTIMATOR_INSTANCES);
 	// add_optional_topic_multi("estimator_aid_src_terrain_optical_flow", 100, MAX_ESTIMATOR_INSTANCES);
 	// add_optional_topic_multi("estimator_aid_src_ev_yaw", 100, MAX_ESTIMATOR_INSTANCES);
+	add_topic_multi("estimator_aid_src_aux_global_position", 0, MAX_ESTIMATOR_INSTANCES);
+	add_topic("vehicle_visual_odometry");
+	add_topic("global_odometry");
 
 	// log all raw sensors at minimal rate (at least 1 Hz)
 	add_topic_multi("battery_status", 200, 2);

mattmccrink/PX4_1_13_3

src/modules/ekf2/EKF/aux_global_position.cpp
@@ -1,0 +1,103 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2023 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "ekf.h"
+
+#include "aux_global_position.hpp"
+
+#if defined(CONFIG_EKF2_AUX_GLOBAL_POSITION) && defined(MODULE_NAME)
+
+void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed)
+{
+
+#if defined(MODULE_NAME)
+
+	if (_aux_global_position_sub.updated()) {
+
+		aux_global_position_s aux_global_position{};
+		_aux_global_position_sub.copy(&aux_global_position);
+
+		const int64_t time_us = aux_global_position.timestamp_sample - static_cast<int64_t>(_param_ekf2_agp_delay.get() * 1000);
+
+		AuxGlobalPositionSample sample{};
+		sample.time_us = time_us;
+		sample.latitude = aux_global_position.latitude;
+		sample.longitude = aux_global_position.longitude;
+		sample.positional_uncertainty = aux_global_position.positional_uncertainty;
+		sample.position_valid = aux_global_position.valid;
+		sample.altitude = aux_global_position.altitude_agl;
+		sample.heading = aux_global_position.heading;
+		sample.heading_valid = aux_global_position.heading_valid;
+
+		_aux_global_position_buffer.push(sample);
+	}
+
+#endif // MODULE_NAME
+
+
+	AuxGlobalPositionSample aux_global_position_sample;
+
+	if (_aux_global_position_buffer.pop_first_older_than(imu_delayed.time_us, &aux_global_position_sample)) {
+
+		if (!ekf.global_origin_valid()) {
+			return;
+		}
+
+		const Vector2f position = ekf.global_origin().project(aux_global_position_sample.latitude, aux_global_position_sample.longitude);
+		//const float hgt = ekf.getEkfGlobalOriginAltitude() - (float)aux_global_position_sample.altitude;
+
+		estimator_aid_source2d_s aid_src{};
+
+		// relax the upper observation noise limit which prevents bad GPS perturbing the position estimate
+		float pos_noise = math::max(aux_global_position_sample.positional_uncertainty, _param_ekf2_agp_noise.get());
+		const float pos_var = sq(pos_noise);
+		const Vector2f pos_obs_var(pos_var, pos_var);
+		ekf.updateHorizontalPositionAidSrcStatus(aux_global_position_sample.time_us,
+				position,                                   // observation
+				pos_obs_var,                                // observation variance
+				math::max(_param_ekf2_agp_gate.get(), 1.f), // innovation gate
+				aid_src);
+		aid_src.fusion_enabled = _param_ekf2_agp_ctrl.get() & 0b001; // bit 0 Horizontal position
+
+		if (aux_global_position_sample.position_valid && ekf.control_status_flags().yaw_align) {
+			ekf.fuseHorizontalPosition(aid_src);
+		}
+
+#if defined(MODULE_NAME)
+		aid_src.timestamp = hrt_absolute_time();
+		_estimator_aid_src_aux_global_position_pub.publish(aid_src);
+#endif // MODULE_NAME
+	}
+}
+
+#endif // CONFIG_EKF2_AUX_GLOBAL_POSITION

src/modules/ekf2/EKF/aux_global_position.hpp
@@ -1,0 +1,113 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2023 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef EKF_AUX_GLOBAL_POSITION_HPP
+#define EKF_AUX_GLOBAL_POSITION_HPP
+
+// interface?
+//  - ModuleParams
+//  - Base class EKF
+//  -  bool update(imu)
+//   how to get delay?
+//  WelfordMean for init?
+//  WelfordMean for rate
+
+#include "common.h"
+#include "RingBuffer.h"
+
+#if defined(CONFIG_EKF2_AUX_GLOBAL_POSITION) && defined(MODULE_NAME)
+
+#if defined(MODULE_NAME)
+# include <px4_platform_common/module_params.h>
+# include <uORB/PublicationMulti.hpp>
+# include <uORB/Subscription.hpp>
+# include <uORB/topics/estimator_aid_source2d.h>
+# include <uORB/topics/aux_global_position.h>
+#endif // MODULE_NAME
+
+class Ekf;
+
+class AuxGlobalPosition : public ModuleParams
+{
+public:
+	AuxGlobalPosition() : ModuleParams(nullptr) {}
+	~AuxGlobalPosition() = default;
+
+	void update(Ekf &ekf, const estimator::imuSample &imu_delayed);
+
+	void updateParameters()
+	{
+		updateParams();
+	}
+
+private:
+
+	struct AuxGlobalPositionSample {
+		uint64_t time_us{};     ///< timestamp of the measurement (uSec)
+		double latitude{};
+		double longitude{};
+		float positional_uncertainty{}; ///< XYZ position uncertainty (m)
+		bool position_valid{false};
+		float altitude{};
+		float heading{};      ///< quaternion defining rotation from body to earth frame
+		bool heading_valid{false};
+	};
+
+	RingBuffer<AuxGlobalPositionSample> _aux_global_position_buffer{20}; // TODO: size with _obs_buffer_length and actual publication rate
+	uint64_t _time_last_buffer_push{0};
+
+	enum state {
+		stopped,
+		starting,
+		active,
+		stopping,
+		resetting,
+	};
+
+#if defined(MODULE_NAME)
+	uORB::PublicationMulti<estimator_aid_source2d_s> _estimator_aid_src_aux_global_position_pub{ORB_ID(estimator_aid_src_aux_global_position)};
+	uORB::Subscription _aux_global_position_sub{ORB_ID(aux_global_position)};
+
+	DEFINE_PARAMETERS(
+		(ParamInt<px4::params::EKF2_AGP_CTRL>) _param_ekf2_agp_ctrl,
+		(ParamFloat<px4::params::EKF2_AGP_DELAY>) _param_ekf2_agp_delay,
+		(ParamFloat<px4::params::EKF2_AGP_NOISE>) _param_ekf2_agp_noise,
+		(ParamFloat<px4::params::EKF2_AGP_GATE>) _param_ekf2_agp_gate
+	)
+
+#endif // MODULE_NAME
+};
+
+#endif // CONFIG_EKF2_AUX_GLOBAL_POSITION
+
+#endif // !EKF_AUX_GLOBAL_POSITION_HPP

src/modules/logger/logged_topics.cpp
@@ -201,6 +201,9 @@ void LoggedTopics::add_default_topics()
 	// add_optional_topic_multi("estimator_aid_src_optical_flow", 100, MAX_ESTIMATOR_INSTANCES);
 	// add_optional_topic_multi("estimator_aid_src_terrain_optical_flow", 100, MAX_ESTIMATOR_INSTANCES);
 	// add_optional_topic_multi("estimator_aid_src_ev_yaw", 100, MAX_ESTIMATOR_INSTANCES);
+	add_topic_multi("estimator_aid_src_aux_global_position", 0, MAX_ESTIMATOR_INSTANCES);
+	add_topic("vehicle_visual_odometry");
+	add_topic("aux_global_position");
 
 	// log all raw sensors at minimal rate (at least 1 Hz)
 	add_topic_multi("battery_status", 200, 2);