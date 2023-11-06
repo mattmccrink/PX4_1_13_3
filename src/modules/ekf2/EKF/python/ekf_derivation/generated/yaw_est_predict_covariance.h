3ec58b91d919067e0002f13be62199e434fa8656
bresch <[brescianimathieu@gmail.com](mailto:brescianimathieu@gmail.com)>
2023-11-06 16:31:27 +0100

    yaw_est: use error-state covariance prediction
    Convergence improvements in high yaw rate conditions

--- src/modules/ekf2/EKF/python/ekf_derivation/generated/yaw_est_predict_covariance.h
+++ src/modules/ekf2/EKF/python/ekf_derivation/generated/yaw_est_predict_covariance.h
@@ -20,6 +20,7 @@ namespace sym {
  *     P: Matrix33
  *     d_vel: Matrix21
  *     d_vel_var: Scalar
+ *     d_ang: Scalar
  *     d_ang_var: Scalar
  *
  * Outputs:
@@ -29,13 +30,13 @@ template <typename Scalar>
 void YawEstPredictCovariance(const matrix::Matrix<Scalar, 3, 1>& state,
                              const matrix::Matrix<Scalar, 3, 3>& P,
                              const matrix::Matrix<Scalar, 2, 1>& d_vel, const Scalar d_vel_var,
-                             const Scalar d_ang_var,
+                             const Scalar d_ang, const Scalar d_ang_var,
                              matrix::Matrix<Scalar, 3, 3>* const P_new = nullptr) {
-  // Total ops: 33
+  // Total ops: 52
 
   // Input arrays
 
-  // Intermediate terms (7)
+  // Intermediate terms (11)
   const Scalar _tmp0 = std::cos(state(2, 0));
   const Scalar _tmp1 = std::sin(state(2, 0));
   const Scalar _tmp2 = -_tmp0 * d_vel(1, 0) - _tmp1 * d_vel(0, 0);
@@ -44,6 +45,10 @@ void YawEstPredictCovariance(const matrix::Matrix<Scalar, 3, 1>& state,
       std::pow(_tmp0, Scalar(2)) * d_vel_var + std::pow(_tmp1, Scalar(2)) * d_vel_var;
   const Scalar _tmp5 = _tmp0 * d_vel(0, 0) - _tmp1 * d_vel(1, 0);
   const Scalar _tmp6 = P(1, 2) + P(2, 2) * _tmp5;
+  const Scalar _tmp7 = _tmp0 * d_ang;
+  const Scalar _tmp8 = _tmp1 + _tmp7;
+  const Scalar _tmp9 = _tmp0 - _tmp1 * d_ang;
+  const Scalar _tmp10 = -_tmp8 * (-_tmp1 - _tmp7) + std::pow(_tmp9, Scalar(2));
 
   // Output terms (1)
   if (P_new != nullptr) {
@@ -55,9 +60,10 @@ void YawEstPredictCovariance(const matrix::Matrix<Scalar, 3, 1>& state,
     _p_new(0, 1) = P(0, 1) + P(2, 1) * _tmp2 + _tmp3 * _tmp5;
     _p_new(1, 1) = P(1, 1) + P(2, 1) * _tmp5 + _tmp4 + _tmp5 * _tmp6;
     _p_new(2, 1) = 0;
-    _p_new(0, 2) = _tmp3;
-    _p_new(1, 2) = _tmp6;
-    _p_new(2, 2) = P(2, 2) + d_ang_var;
+    _p_new(0, 2) = _tmp10 * _tmp3;
+    _p_new(1, 2) = _tmp10 * _tmp6;
+    _p_new(2, 2) = P(2, 2) * std::pow(_tmp10, Scalar(2)) +
+                   d_ang_var * std::pow(Scalar(-_tmp0 * _tmp9 - _tmp1 * _tmp8), Scalar(2));
   }
 }  // NOLINT(readability/fn_size)
 
